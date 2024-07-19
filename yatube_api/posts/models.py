6120c9363dfc821580362a96e3ff7dfdfab7deef
Андрей Пискунов <piskunov2302@yandex.ru>
2024-07-19 21:56:43 +0300

    First commit!

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+MAXIMUM_STRING_LENGTH = 256
+
+NUMBER_OF_VISIBLE_CHARACTERS = 15
+
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=MAXIMUM_STRING_LENGTH)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title[:NUMBER_OF_VISIBLE_CHARACTERS]
 
 
 class Post(models.Model):
@@ -11,9 +24,23 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = (
+            'group',
+            'pub_date',
+            'author__username',
+        )
 
     def __str__(self):
-        return self.text
+        return self.text[:NUMBER_OF_VISIBLE_CHARACTERS]
 
 
 class Comment(models.Model):
@@ -24,3 +51,30 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text[:NUMBER_OF_VISIBLE_CHARACTERS]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_subscriber_author_pair'
+            )
+        ]
+
+    def __str__(self):
+        return f'follower:{self.user}, following:{self.following}'
